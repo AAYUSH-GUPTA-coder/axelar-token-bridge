86e08b85f1bbeab59f8fa7285773ef304e20d5f4
Aayush Gupta <guptaaayush432@gmail.com>
2023-05-25 22:05:30 +0530

    [deploy] added deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,32 +1,31 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
+require("@nomiclabs/hardhat-etherscan");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const SendToken = await hre.ethers.getContractFactory("SendToken");
+  const sendToken = await SendToken.deploy(); 
+  await sendToken.deployed();
+  console.log("sendToken deployed to:", sendToken.address);
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+  console.log("Sleeping.....");
+  // Wait for etherscan to notice that the sendToken has been deployed
+  await sleep(100000); // 100 seconds
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  // Verify the sendToken after deploying
+  await hre.run("verify:verify", {
+    address: sendToken.address,
+    constructorArguments: [],
+    contract: "contracts/SendToken.sol:SendToken",
+  });
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
