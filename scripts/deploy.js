241d5abf66e98c4c3e128ae2d8a8ab889b24a182
Aayush Gupta <guptaaayush432@gmail.com>
2023-06-21 16:18:47 +0530

    [script] added owner variable

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,10 +4,11 @@ require("@nomiclabs/hardhat-etherscan");
 // variables value for Mumbai testnetwork
 const IAxelarGateway = "0xBF62ef1486468a6bd26Dd669C06db43dEd5B849B";
 const tokenAddress = "0x2c852e740B62308c46DD29B982FBb650D063Bd07";
+const owner = "0x4E476F7FB84c785557cDECdbf8CADEbE8EA57C37"
 
 async function main() {
   const SendToken = await hre.ethers.getContractFactory("SendToken");
-  const sendToken = await SendToken.deploy(IAxelarGateway, tokenAddress); 
+  const sendToken = await SendToken.deploy(IAxelarGateway, tokenAddress, owner); 
   await sendToken.deployed();
   console.log("sendToken deployed to:", sendToken.address);
 
@@ -18,7 +19,7 @@ async function main() {
   // Verify the sendToken after deploying
   await hre.run("verify:verify", {
     address: sendToken.address,
-    constructorArguments: [IAxelarGateway, tokenAddress],
+    constructorArguments: [IAxelarGateway, tokenAddress, owner],
     contract: "contracts/SendToken.sol:SendToken",
   });
 }
